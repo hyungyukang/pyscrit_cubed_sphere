571e20892afce876bc4114e02d8958e9ed9353a8
hyungyukang <kangh@ornl.gov>
2021-04-13 15:18:33 -0400

    Some array bug fixed

--- EXE_Cubed_sphere_surf_color_topo.py
+++ EXE_Cubed_sphere_surf_color_topo.py
@@ -1,3 +1,4 @@
+#from mpl_toolkits import mplot3d
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -15,13 +16,14 @@ from function_Cubed_sphere_grid_equiang import function_Cubed_sphere_grid_equian
 
 
 No =3    # Polynomial Order
-Ng = No + 1  # GLL points
 Ne = 4       # #Element
-Nge = Ng*Ne
 IB = 501  # Resolution
+
+Ng = No + 1  # GLL points
+Nge = Ng*Ne
 Nfaces = 6
 
-pi180  = np.pi/180
+#------------------------------------------------------------------------
 
 x = np.zeros([IB,Nge,Nfaces])
 y = np.zeros([IB,Nge,Nfaces])
@@ -29,16 +31,43 @@ z = np.zeros([IB,Nge,Nfaces])
 
 x,y,z = function_Cubed_sphere_grid_equiang (No,Ne,IB)
 
+fig,ax = plt.subplots(1,figsize=(6,5),constrained_layout=True)
+#fig = plt.subplots(figsize=(6,5),constrained_layout=True)
+#------------------------------------------------------------------------
 
 # Display ----------------------------------------------------
 
-fig,ax = plt.subplots(1,figsize=(6,5),constrained_layout=True)
+#ax = plt.axes(projection="3d")
 
 for ip in range(Nfaces):
     for k in range(Nge):
-        ax.scatter(x[:,k,ip],y[:,k,ip],z[:,k,ip],color='k')
-        ax.scatter(y[:,k,ip],x[:,k,ip],z[:,k,ip],color='k')
-        ax.scatter(z[:,k,ip],x[:,k,ip],y[:,k,ip],color='k')
+        #ax.scatter3D(x,y,z,s=10)
+        ax.scatter(x[:,k,ip],y[:,k,ip],z[:,k,ip],c='k')
+        ax.scatter(y[:,k,ip],x[:,k,ip],z[:,k,ip],c='k')
+        ax.scatter(z[:,k,ip],x[:,k,ip],y[:,k,ip],c='k')
+
+#------------------------------------------------------------------------
+# Another set for red colored grid
+
+No = 1    # Polynomial Order
+#Ne = 3       # #Element
+IB = 1001  # Resolution
+
+Ng = No + 1  # GLL points
+Nge = Ng*Ne
+
+xr = np.zeros([IB,Nge,Nfaces])
+yr = np.zeros([IB,Nge,Nfaces])
+zr = np.zeros([IB,Nge,Nfaces])
+
+xr,yr,zr = function_Cubed_sphere_grid_equiang (No,Ne,IB)
+
+
+for ip in range(Nfaces):
+    for k in range(Nge):
+        ax.scatter(xr[:,k,ip],yr[:,k,ip],zr[:,k,ip],color='r')
+        ax.scatter(yr[:,k,ip],xr[:,k,ip],zr[:,k,ip],color='r')
+        ax.scatter(zr[:,k,ip],xr[:,k,ip],yr[:,k,ip],color='r')
 
 ax.axis('equal')
 plt.savefig("fig_test.png")
--- function_Cubed_sphere_grid_equiang.py
+++ function_Cubed_sphere_grid_equiang.py
@@ -26,21 +26,21 @@ def legendre_gauss_lobatto(a,b,N):
     apb= (a+b)/2.0
     bma= (b-a)/2.0
     
-    nh= np.floor(N+1)/2.0
-    Np= N+1
+    #nh= np.floor(N+1)/2.0
+    #Np= N+1
     
     xg= np.zeros([N])
     wg= np.zeros([N])
     xx=0
     
     xg[0]= -1
-    wg[0]=2/(N*(N-1))
-    xg[N-1]= +1
-    wg[N-1]=2/(N*(N-1))
+    wg[0]=2.0/(N*(N-1))
+    xg[N-1]= +1.0
+    wg[N-1]=2.0/(N*(N-1))
 
     for i in range(1,N-1):
         #x= cos( (2*i-1)*pi/(2*N+1) );
-        x=(1-(3*(N-2))/(8*(N-1)**3)) * np.cos(np.pi*(4*i-3)/(4*(N-1)+1))
+        x=(1.0-(3.0*(N-2.0))/(8.0*(N-1)**3)) * np.cos(np.pi*(4.0*(i+1)-3)/(4.0*(N-1)+1))
     
         for j in range(100):
             P0=1
@@ -56,19 +56,19 @@ def legendre_gauss_lobatto(a,b,N):
                 dpp1= dpp
                 dppp1= dppp
          
-                P0= (2*n-1)*x*P1/n - (n-1)*P2/n
-                dp= n*(P1-x*P0)/(1-x*x) # the same with pp= (n/(x*x-1))*(x*P0-P1)
-                dpp= ( 2*x*dp - n*(n+1)*P0   )/(1-x*x)
-                dppp= ( 2*x*dpp-(n*(n+1)-2)*dp)/(1-x*x)
-            xx= x-(2*dp1*dpp1)/(2*dpp1*dpp1 - dp1*dppp1)
+                P0= (2.0*n-1)*x*P1/n - (n-1)*P2/n
+                dp= n*(P1-x*P0)/(1.0-x*x) # the same with pp= (n/(x*x-1))*(x*P0-P1)
+                dpp= ( 2.0*x*dp - n*(n+1)*P0   )/(1.0-x*x)
+                dppp= ( 2.0*x*dpp-(n*(n+1)-2.0)*dp)/(1.0-x*x)
+            xx= x-(2.0*dp1*dpp1)/(2.0*dpp1*dpp1 - dp1*dppp1)
             if (np.abs(xx-x)<10.e-15):
                break
             x= xx
  
-        xg[Np-i-1]= x*bma+apb
-        xg[   i-1]= -xg[Np-i-1]
-        wg[Np-i-1]= 2.0/(N*(N-1)*P1*P1)
-        wg[   i-1]=  wg[Np-i-1]
+        xg[N-i]= x*bma+apb
+        xg[  i]= -xg[N-i]
+        wg[N-i]= 2.0/(N*(N-1)*P1*P1)
+        wg[  i]=  wg[N-i]
 
     #x=xg
     #w=wg
@@ -95,14 +95,14 @@ def function_Cubed_sphere_grid_equiang (No,Ne,IB):
    pi2 = np.pi*2.0
    
 
-   xg= np.zeros([Ng])
-   wg= np.zeros([Ng])
+   xg= np.zeros(Ng)
+   wg= np.zeros(Ng)
    
    #%%%%%%%%%%%%   Call GLL subroutine %%%%%%%%%%%%%
    xg, wg = legendre_gauss_lobatto(-1,+1,Ng)
    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-   ELEp = np.zeros(Ne+1)
+   ELEp = np.zeros(Ne)
    Eratio = np.zeros([Ng,Ne])
    xgrid = np.zeros(IB)
    xcoord = np.zeros([Nge,IB])
@@ -116,40 +116,30 @@ def function_Cubed_sphere_grid_equiang (No,Ne,IB):
    z = np.zeros([IB,Nge,Nfaces])
    
    # For elements line  ( -pi/4 ~ pi/4 )
-   for n in range (Ne+1):
-         ELEp[n] = -np.pi/4 + (n-1)*de_ang
-   #     ELEp[n] = -1 + (n-1)*de_x
+   for n in range (Ne):
+         ELEp[n] = -np.pi/4 + n*de_ang
+   #     ELEp[n] = -1 + n*de_x # For equidis
    
    for k in range (Ng):
       for n in range (Ne):
-          Eratio[n,k] = ELEp[n]+de_ang/2  + xg[k]*(de_ang/2)
-      #   Eratio[n,k] = ELEp[n]+de_x/2  + xg[k]*(de_x/2)
+          Eratio[k,n] = ELEp[n]+de_ang/2  + xg[k]*(de_ang/2)
+      #   Eratio[n,k] = ELEp[n]+de_x/2  + xg[k]*(de_x/2) # For equidis
    
    for i in range(IB):
-        xgrid[i] = -np.pi/4 + (i-1)*dthe 
-   #    xgrid[i] = -1.0 + (i-1)*dx 
+        xgrid[i] = -np.pi/4 + i*dthe 
+   #    xgrid[i] = -1.0 + i*dx  # For equidis
    
    ic = 0
    for n in range(Ne):
        for k in range(Ng):
            for i in range(IB):
-               xcoord[ic,i] = Eratio[n,k]
+               xcoord[ic,i] = Eratio[k,n]
            ic = ic + 1
 
-#  %for k = 1:Nge
-#      %plot(xgrid(k,:)) ; hold on;
-#     % scatter(xcoord(k,:),xgrid(:),'filled','b','linewidth',1) ; hold on; 
-#     % scatter(xgrid(:),xcoord(k,:),'filled','b','linewidth',1) ; hold on; 
-#  
-#  %   scatter(xcoord(k,:),xgrid(:),2,'filled','b','linewidth',2) ; hold on; 
-#  %   scatter(xgrid(:),xcoord(k,:),2,'filled','b','linewidth',2) ; hold on; 
-#  %end
-#  axis equal;
-   
    ic = 0
    for n in range(Ne):
        for k in range(Ng):
-           cart_y[ic] = a*np.tan(Eratio[n,k])
+           cart_y[ic] = a*np.tan(Eratio[k,n])
            ic = ic + 1
    
    for j in range(Nge):
@@ -160,19 +150,19 @@ def function_Cubed_sphere_grid_equiang (No,Ne,IB):
    for ip in range(Nfaces):
        for j in range(Nge):
            for i in range(IB):
-               if (ip == 1):
-                  sphere_lat[i,j,ip]=  np.arcsin(cart_y[j]/cart_r[i,j])
+               if (ip == 0):
+                  sphere_lat[i,j,ip]= np.arcsin(cart_y[j]/cart_r[i,j])
                   sphere_lon[i,j,ip]= np.arctan2(cart_x[i,j], a)
-               elif(ip == 2):
-                  sphere_lat[i,j,ip]=  np.arcsin(cart_y[j]/cart_r[i,j])
+               elif(ip == 1):
+                  sphere_lat[i,j,ip]= np.arcsin(cart_y[j]/cart_r[i,j])
                   sphere_lon[i,j,ip]= np.arctan2(a,-cart_x[i,j])
-               elif (ip == 3):
-                  sphere_lat[i,j,ip]=  np.arcsin(cart_y[j]/cart_r[i,j])
+               elif (ip == 2):
+                  sphere_lat[i,j,ip]= np.arcsin(cart_y[j]/cart_r[i,j])
                   sphere_lon[i,j,ip]= np.arctan2(-cart_x[i,j],-a)
-               elif (ip == 4):
-                  sphere_lat[i,j,ip]=  np.arcsin(cart_y[j]/cart_r[i,j])
+               elif (ip == 3):
+                  sphere_lat[i,j,ip]= np.arcsin(cart_y[j]/cart_r[i,j])
                   sphere_lon[i,j,ip]= np.arctan2(-a,cart_x[i,j])
-               elif (ip == 5):
+               elif (ip == 4):
                   yy = np.abs(cart_y[j])
                   xx = np.abs(cart_x[i,j])
                   if (xx > dist_limit or yy > dist_limit):
@@ -180,7 +170,7 @@ def function_Cubed_sphere_grid_equiang (No,Ne,IB):
                   else:
                      sphere_lon[i,j,ip]= 0.0
                   sphere_lat[i,j,ip]=  np.arcsin( a/cart_r[i,j]);
-               elif (ip == 6):
+               elif (ip == 5):
                   yy = np.abs(cart_y[j]);
                   xx = np.abs(cart_x[i,j]);
                   if (xx > dist_limit or yy > dist_limit): 
